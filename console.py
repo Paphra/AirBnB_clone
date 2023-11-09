15a2f9249d277f7a271640926dd4802fe2b0fa6a
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-11-09 08:24:51 +0300

    Finished the implementation of Task #3 - Added the class `BaseModel` in `models/base_model.py` - Added all the instance attributes to the class (id, created_at, updated_at) - Added the `__str__` method that returns the string representation of the class - Added the methods 	-- `save(self)` that updates the `updated_at` attribute 	-- `to_dict(self)` that returns the dictionary representation

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 """
 
 import cmd
+from models.base_model import BaseModel
 
 
 class Console(cmd.Cmd):
@@ -13,6 +14,25 @@ class Console(cmd.Cmd):
     """
 
     prompt = "(hbnb) "
+
+    def do_test_base_task_3(self, line):
+        """Test the BaseModel class
+        """
+
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+        print(my_model)
+        my_model.save()
+        print(my_model)
+        my_model_json = my_model.to_dict()
+        print(my_model_json)
+        print("JSON of my_model:")
+        for key in my_model_json.keys():
+            print("\t{}: ({}) - {}".format(
+                key,
+                type(my_model_json[key]),
+                my_model_json[key]))
 
     def do_EOF(self, line):
         """End of File Method
--- models/base_model.py
+++ models/base_model.py
@@ -3,3 +3,50 @@
 
 Contains the BaseModel class
 """
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel():
+    """The Model from which all other models inherit
+    """
+
+    def __init__(self):
+        """The method that initializes properties
+        """
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns a string representation of the Model
+        """
+
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__
+        )
+
+    def save(self):
+        """Updates the updated_at attr with current datetime
+        """
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Gets the dictionary containing all keys of __dict__
+
+        Returns:
+            dict: the dictionary containing all keys of the instance
+        """
+
+        ret_dict = {}       # dictionary to return
+        for k, v in self.__dict__.items():
+            if k in ['created_at', 'updated_at']:
+                ret_dict[k] = datetime.isoformat(getattr(self, k))
+            else:
+                ret_dict[k] = v
+        ret_dict['__class__'] = self.__class__.__name__
+
+        return ret_dict
