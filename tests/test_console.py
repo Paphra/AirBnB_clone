e911448f2f46d9e6de9c2350e6db1a9f581afa05
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-11-12 21:53:37 +0300

    Testing the Other values

--- tests/test_console.py
+++ tests/test_console.py
@@ -5,6 +5,9 @@ Contains the tests for the console command interpreter
 """
 
 import unittest
+from unittest.mock import patch
+from io import StringIO
+from console import HBNBCommand
 
 
 class TestConsole(unittest.TestCase):
@@ -20,6 +23,12 @@ class TestConsole(unittest.TestCase):
         """Tear down the tests
         """
         return super().tearDown()
+
+    def test_console_entry(self):
+        """Test the entry to the console
+        """
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("? show")
 
 
 if __name__ == "__main___":
